0cccb2ecfe2773f249f9ef7e4ce233ddaa6e7799
abrath98 <78123421+abrath98@users.noreply.github.com>
2021-01-28 09:28:50 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>PUBGM</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="css/animate-3.7.0.css">
+    <link rel="stylesheet" href="css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="css/owl-carousel.min.css">
+    <link rel="stylesheet" href="css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Area Starts -->
+	<header class="header-area header-area2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10">
+                    <div class="custom-navbar">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div> 
+                    <div class="main-menu main-menu2">
+                        <ul>
+                            <li class="active"><a href="index.php">HOME</a></li>
+                            <li><a href="about.php">MAPS</a></li>
+                            <li><a href="menu.php">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area banner-area2 menu-bg text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1><i>MAPS PUBGM</i></h1>
+                   </div>
+            </div>
+        </div>
+    </section>
+<!-- Banner Area End -->
+
+<!-- Food Area starts -->
+<section class="food-area section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="section-top">
+                    <h3>BEBERAPA MAP YANG ADA DI PUBG MOBILE</h3> 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    <!-- Welcome Area End -->
+    
+    <!-- Deshes Area Starts -->
+    <div class="deshes-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6 align-self-center">
+                    <div class="deshes-text">
+                        <h3>ERANGEL</h3>
+                        <p class="pt-3">Map PUBG Mobile Erangel adalah map pertama yang hadir di game battle royale buatan Tencent Games ini. Map ini merupakan perpaduan antara kota kecil, kota besar, tempat militer, dan pembangkit listrik yang tersebar di daratan hijau yang sangat luas. Map ini juga menawarkan cuaca yang dinamis sehingga membuat permainan menjadi sangat menarik. Karena Erangel ini sangat luas, pemain yang bertugas menjadi sniper punya peran penting. Pertempuran di map ini sering kali terjadi pada jarak jauh.</p>
+                    </div>
+                </div>
+                <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
+                    <img src="images/079987000_1456974986-20160303-Ilustrasi-lari-iStockphoto-1.jpg" alt="" class="img-fluid">
+                </div>
+            </div>
+                      <div class="row">
+                    <div class="col-lg-5 col-md-6 align-self-center">
+                        <div class="deshes-text">
+                            <h3>MIRAMAR</h3>
+                            <p class="pt-3">Hampir sama seperti Erangel, map PUBG Mobile Miramar juga sangat luas. Namun, karakter map ini berbeda dengan Erangel. Jika erangel adalah daerah dengan daratan yang hijau, Miramar adalah sebaliknya. Map ini berupa Kawasan gurun dengan sedikit sekali pepohonan. Pada peta ini, satu-satunya tempat berlindung paling aman adalah bangunan.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
+                        <img src="images/cycling-d12a43cbc32267e398e2857472bc43dd_600x400.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
+                        <img src="images/lompat-tali.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center order-1 order-md-2">
+                        <div class="deshes-text">
+                            <h3>SANHOK</h3>
+                            <p class="pt-3">Map yang satu ini berukuran setengahnya dari Erangel dan Miramar. Sanhok terinspirasi dari hutan hujan yang ada di Kawasan Asia Tenggara. Sanhok didominasi dengan tanaman subur, bangunan, reruntuhan, hingga pangkalan militer. Satu yang paling istimewa adalah map ini mempunyai sistem cuaca yang sangat seru.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
+                        <img src="images/1243888-manfaat-naik-turun-tangga.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center order-1 order-md-2">
+                        <div class="deshes-text">
+                            <h3>VIKENDI</h3>
+                            <p class="pt-3">Vikendi adalah satu-satunya map bersalju di PUBG Mobile. Di peta yang satu ini, pemain bisa memilih apakah ingin memainkan versi siang atau malam. Di Vikendi, ada satu senjata istimewa yang tidak ditemukan di map lain. Senjata yang dimaksud adalah g36C yang merupakan senapan seru.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    </div>
+
+
+    <!-- Javascript -->
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="js/vendor/wow.min.js"></script>
+    <script src="js/vendor/owl-carousel.min.js"></script>
+    <script src="js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="js/vendor/jquery.nice-select.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>PUBGM</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="css/animate-3.7.0.css">
+    <link rel="stylesheet" href="css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="css/owl-carousel.min.css">
+    <link rel="stylesheet" href="css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Area Starts -->
+	<header class="header-area header-area2">
+        <div class="container">
+            <div class="row">
+                 <div class="col-lg-10">
+                    <div class="custom-navbar">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div> 
+                    <div class="main-menu main-menu2">
+                    <ul>
+                            <li class="active"><a href="index.php">HOME</a></li>
+                            <li><a href="about.php">MAPS</a></li>
+                            <li><a href="menu.php">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area banner-area2 menu-bg text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1><i>CONTACT PUBGM</i></h1>
+                   </div>
+            </div>
+        </div>
+    </section>
+    <!-- Banner Area End -->
+
+    <!-- Food Area starts -->
+    <section class="food-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="section-top">
+                        <h3>CONTACT RESMI PUBG MOBILE</h3> 
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="single-food">
+                        <div class="food-img">
+                            <img src="images/1242742-1000xauto-resep-makanan-sehat-untuk-diet.jpg" class="img-fluid" alt="">
+                        </div>
+                         <div class="customer-text">
+                                <h5>KLIK LINK DIBAWAH INI UNTUK MENUJU KONTAK RESMI</h5>
+                                <h5> <a href="https://www.pubgmobile.com/en-US/">-WEBSITE</a> </h5>          
+                                <h5> <a href="https://www.instagram.com/pubgmobile/?hl=id">-FACEBOOK</a> </h5>   
+                             <h5> <a href="https://id-id.facebook.com/PUBGMOBILE">-FACEBOOK</a> </h5>
+                                                  
+    
+    </section>
+    <!-- Food Area End -->
+
+    
+
+    
+
+
+    <!-- Javascript -->
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="js/vendor/wow.min.js"></script>
+    <script src="js/vendor/owl-carousel.min.js"></script>
+    <script src="js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="js/vendor/jquery.nice-select.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
